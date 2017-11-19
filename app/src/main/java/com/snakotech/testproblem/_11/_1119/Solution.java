9235c3c98d77f10d0fc6af2d25fc97aaf3a1c4dc
zhangchuanchuan <543749092@qq.com>
2017-11-19 18:40:02 +0800

    加入推导公式

--- app/src/main/java/com/snakotech/testproblem/_11/_1119/Solution.java
+++ app/src/main/java/com/snakotech/testproblem/_11/_1119/Solution.java
@@ -49,6 +49,12 @@ public class Solution {
 
 
     //一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+
+    /**
+     *  f(n-1) = f(1) + f(2) + ... + f(n-2) + 1;
+     *  f(n) = f(1) + f(2) +...+f(n-1) + 1;
+     *  f(n) = f(n-1) - 1 + f(n-1) + 1 = 2f(n-1)
+     */
     public int JumpFloorII(int target) {
         if (target == 0) {
             return 0;
